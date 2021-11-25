709dec5fa435aa5525d3d6c6a96bea4c4d83bae4
TANAV <tanavsaraf@gmail.com>
2021-11-25 09:30:39 +0530

    helloooo

--- screens/recentPass.js
+++ screens/recentPass.js
@@ -1,68 +1,87 @@
-import * as React from 'react';
-import {View,Text,TextInput,TouchableOpacity,StyleSheet,Modal} from 'react-native';
-export default class RecentPass extends React.Component{
-    constructor(){
-        super()
-        this.state={
-           passcodes:[],
-           captha:'',
-           capthaVeri:'',
-           capthaTrue:false,
-           showModal:true,
-        }
-    }
-    verification=()=>{
-      this.setState({captha:Math.random(0,1).toString(20)});
+import * as React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Modal,
+} from "react-native";
+export default class RecentPass extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      passcodes: [],
+      captha: "",
+      capthaVeri: "",
+      capthaTrue: false,
+      showModal: true,
+    };
+  }
+  verification = () => {
+    this.setState({ captha: Math.random(0, 1).toString(20) });
+    return (
       <Modal visible={this.state.showModal}>
         <Text>{this.state.captha}</Text>
-        <TextInput value={this.state.capthaVeri} onChangeText={()=>{
-          if(this.state.captha==this.state.capthaVeri){
-            this.setState({capthaTrue:true,showModal:false})
-          }else{
-            this.setState({capthaTrue:false})
-          }
-        }} />
+        <TextInput
+          value={this.state.capthaVeri}
+          onChangeText={() => {
+            if (this.state.captha == this.state.capthaVeri) {
+              this.setState({ capthaTrue: true, showModal: false });
+            } else {
+              this.setState({ capthaTrue: false });
+            }
+          }}
+        />
       </Modal>
-    }
-    componentDidMount(){
-      
-      this.verification()
-    }
-    render(){
-        if(this.capthaTrue){
-          return(<View>
-            <Text style={styles.text}>Hello Welcome to View page</Text>
-            <TouchableOpacity 
+    );
+  };
+  componentDidMount() {
+    this.verification();
+  }
+  render() {
+    if (this.capthaTrue) {
+      return (
+        <View>
+          <Text style={styles.text}>Hello Welcome to View page</Text>
+          <TouchableOpacity
             style={styles.button}
-            onPress={()=>{
-                this.props.navigation.navigate("GeneratePasscode")
-            }}><Text style={styles.buttonText}>Click to relocate</Text></TouchableOpacity>
-        </View>)
-        }else{
-          this.verification()
-        }
+            onPress={() => {
+              this.props.navigation.navigate("GeneratePasscode");
+            }}
+          >
+            <Text style={styles.buttonText}>Click to relocate</Text>
+          </TouchableOpacity>
+        </View>
+      );
+    } else {
+      return null
+      {
+        this.verification()
+      }
     }
-}const styles = StyleSheet.create({
-    button: {
-      backgroundColor: "grey",
-      alignItems: "center",
-      width: 190,
-      height: 50,
-      alignSelf: "center",
-      justifyContent:'center',
-      borderRadius:2,
-      borderWidth:2
-    },
-    text: {
-      textAlign:'center',
-      marginTop: 50,
-      padding: 20,
-      fontSize: 30,
-      
-    },
-    buttonText: {
-      textAlign:'center',
-      
-      color:'orange',
-    },
-  });
+  }
+}
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "grey",
+    alignItems: "center",
+    width: 190,
+    height: 50,
+    alignSelf: "center",
+    justifyContent: "center",
+    borderRadius: 2,
+    borderWidth: 2,
+  },
+  text: {
+    textAlign: "center",
+    marginTop: 50,
+    padding: 20,
+    fontSize: 30,
+  },
+  buttonText: {
+    textAlign: "center",
+
+    color: "orange",
+  },
+});
